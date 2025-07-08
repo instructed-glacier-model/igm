215fbe2f57509fdecc828293d191497f2e1aaeea
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-07-08 10:47:06 +0200

    merge compute_strainrate_Glen_2layers_tf

--- igm/processes/iceflow/energy/cost_shear.py
+++ igm/processes/iceflow/energy/cost_shear.py
@@ -59,16 +59,16 @@ def compute_sr2(dUdx, dVdx, dUdy, dVdy, dUdz, dVdz):
     return 0.5 * ( Exx**2 + Exy**2 + Exy**2 + Eyy**2 + Ezz**2 + Exz**2 + Eyz**2 + Exz**2 + Eyz**2 )
 
 @tf.function()
-def compute_strainrate_Glen_tf(U, V, thk, slidingco, dX, ddz, sloptopgx, sloptopgy, thr):
+def compute_strainrate_Glen_tf(dUdx, dVdx, dUdy, dVdy, Um, Vm, thk, slidingco, dX, ddz, usurf, thr):
+
+    sloptopgx, sloptopgy = compute_gradient_stag(usurf - thk, dX, dX)
+    sloptopgx = tf.expand_dims(sloptopgx, axis=1)
+    sloptopgy = tf.expand_dims(sloptopgy, axis=1)
+    # TODO : sloptopgx, sloptopgy must be the elevaion of layers! not the bedrock,
+    #  this probably has very little effects.
     
-    dUdx, dVdx, dUdy, dVdy = compute_horizontal_strainrate_Glen_tf(U, V, dX)
-
-    # compute the horizontal average, these quantitites will be used for vertical derivatives
-    Um = stag4(U)
-    Vm = stag4(V)
-
     # homgenize sizes in the vertical plan on the stagerred grid
-    if U.shape[1] > 1:
+    if Um.shape[1] > 1:
         dUdx = (dUdx[:, :-1, :, :] + dUdx[:, 1:, :, :]) / 2
         dVdx = (dVdx[:, :-1, :, :] + dVdx[:, 1:, :, :]) / 2
         dUdy = (dUdy[:, :-1, :, :] + dUdy[:, 1:, :, :]) / 2
@@ -92,66 +92,10 @@ def compute_strainrate_Glen_tf(U, V, thk, slidingco, dX, ddz, sloptopgx, sloptop
     dVdy = dVdy - dVdz * sloptopgy
 
     return compute_sr2(dUdx, dVdx, dUdy, dVdy, dUdz, dVdz)
- 
-@tf.function()
-def _cost_shear(U, V, thk, usurf, arrhenius, slidingco, dX, dz, 
-                    exp_glen, regu_glen, thr_ice_thk, min_sr, max_sr):
-    
-    COND = ( (thk[:, 1:, 1:] > 0) & (thk[:, 1:, :-1] > 0)
-            & (thk[:, :-1, 1:] > 0) & (thk[:, :-1, :-1] > 0) )
-    COND = tf.expand_dims(COND, axis=1)
-  
-    # B has Unit Mpa y^(1/n)
-    B = 2.0 * arrhenius ** (-1.0 / exp_glen)
 
-    sloptopgx, sloptopgy = compute_gradient_stag(usurf - thk, dX, dX)
-    sloptopgx = tf.expand_dims(sloptopgx, axis=1)
-    sloptopgy = tf.expand_dims(sloptopgy, axis=1)
-    # TODO : sloptopgx, sloptopgy must be the elevaion of layers! not the bedrock,
-    #  this probably has very little effects.
-
-    p = 1.0 + 1.0 / exp_glen
-
-    # sr has unit y^(-1)
-    sr2 = compute_strainrate_Glen_tf(
-        U, V, thk, slidingco, dX, dz, sloptopgx, sloptopgy, thr=thr_ice_thk
-    )
-     
-    sr2 = tf.where(COND, sr2, 0.0)
-    
-    sr2capped = tf.clip_by_value(sr2, min_sr**2, max_sr**2)
-
-    sr2capped = tf.where(COND, sr2capped, 0.0)
-
-    p_term = ((sr2capped + regu_glen**2) ** ((p-2) / 2)) * sr2
-  
-    # C_shear is unit  Mpa y^(1/n) y^(-1-1/n) * m = Mpa m/y
-    if len(B.shape) == 3:
-        C_shear = stag4(B) * tf.reduce_sum(dz * p_term, axis=1 ) / p
-    else:
-        C_shear = tf.reduce_sum( stag8(B) * dz * p_term, axis=1 ) / p
-
-    return C_shear
-
-
-# In the case of a 2 layers model, we assume a velcity profile is a SIA-like profile
-@tf.function()
-def _cost_shear_2layers(thk, arrhenius, U, V, dX, exp_glen, regu_glen):
-
-    n, w = gauss_points_and_weights(ord_gauss=3)
-
-    dUdx, dVdx, dUdy, dVdy = compute_horizontal_strainrate_Glen_tf(U, V, dX)
-
-    Um = stag4(U)
-    Vm = stag4(V)
-
-    # B has Unit Mpa y^(1/n)
-    B = 2.0 * arrhenius ** (-1.0 / exp_glen)
-    p = 1.0 + 1.0 / exp_glen
- 
-    zeta = n[None,:,None,None]
-    weight = w[None,:,None,None]
-
+tf.function()
+def compute_strainrate_Glen_2layers_tf(dUdx, dVdx, dUdy, dVdy, Um, Vm, thk, zeta, exp_glen):
+        
     UDX = tf.expand_dims(dUdx[:, 0, :, :],1) \
         + tf.expand_dims(dUdx[:, -1, :, :]-dUdx[:, 0, :, :],1) * psia(zeta,exp_glen)
     VDY = tf.expand_dims(dVdy[:, 0, :, :],1) \
@@ -166,11 +110,66 @@ def _cost_shear_2layers(thk, arrhenius, U, V, dX, exp_glen, regu_glen):
     VDZ = tf.expand_dims(Vm[:, -1, :, :]-Vm[:, 0, :, :],1) \
         * psiap(zeta,exp_glen) / tf.maximum( stag4(thk) , 1)
         
-    sr2 = compute_sr2(UDX, VDX, UDY, VDY, UDZ, VDZ)
+    return compute_sr2(UDX, VDX, UDY, VDY, UDZ, VDZ)
+ 
+@tf.function()
+def _cost_shear(U, V, thk, usurf, arrhenius, slidingco, dX, dz, 
+                    exp_glen, regu_glen, thr_ice_thk, min_sr, max_sr):
+    
+    # B has Unit Mpa y^(1/n)
+    B = 2.0 * arrhenius ** (-1.0 / exp_glen)
+    p = 1.0 + 1.0 / exp_glen
+
+    dUdx, dVdx, dUdy, dVdy = compute_horizontal_strainrate_Glen_tf(U, V, dX)
+
+    # compute the horizontal average, these quantitites will be used for vertical derivatives
+    Um = stag4(U)
+    Vm = stag4(V)
+
+    # sr has unit y^(-1)
+    sr2 = compute_strainrate_Glen_tf(
+        dUdx, dVdx, dUdy, dVdy, Um, Vm, thk, slidingco, dX, dz, usurf, thr=thr_ice_thk
+    )
+
+    
+    COND = ( (thk[:, 1:, 1:] > 0) & (thk[:, 1:, :-1] > 0)
+            & (thk[:, :-1, 1:] > 0) & (thk[:, :-1, :-1] > 0) )
+    COND = tf.expand_dims(COND, axis=1)
+     
+    sr2 = tf.where(COND, sr2, 0.0)
+    
+    sr2capped = tf.clip_by_value(sr2, min_sr**2, max_sr**2)
+
+    sr2capped = tf.where(COND, sr2capped, 0.0)
+
+    p_term = ((sr2capped + regu_glen**2) ** ((p-2) / 2)) * sr2
+  
+    # C_shear is unit  Mpa y^(1/n) y^(-1-1/n) * m = Mpa m/y
+    if len(B.shape) == 3:
+        return stag4(B) * tf.reduce_sum(dz * p_term, axis=1 ) / p
+    else:
+        return tf.reduce_sum( stag8(B) * dz * p_term, axis=1 ) / p
+ 
+# In the case of a 2 layers model, we assume a velcity profile is a SIA-like profile
+@tf.function()
+def _cost_shear_2layers(thk, arrhenius, U, V, dX, exp_glen, regu_glen):
+
+    # B has Unit Mpa y^(1/n)
+    B = 2.0 * arrhenius ** (-1.0 / exp_glen)
+    p = 1.0 + 1.0 / exp_glen
+
+    dUdx, dVdx, dUdy, dVdy = compute_horizontal_strainrate_Glen_tf(U, V, dX)
+
+    Um = stag4(U)
+    Vm = stag4(V)
+
+    n, w = gauss_points_and_weights(ord_gauss=3)
+    zeta = n[None,:,None,None]
+    weight = w[None,:,None,None]
+
+    sr2 = compute_strainrate_Glen_2layers_tf(dUdx, dVdx, dUdy, dVdy, Um, Vm, thk, zeta, exp_glen)
 
     p_term = (sr2 + regu_glen**2) ** (p / 2) / p
   
     # C_shear is unit  Mpa y^(1/n) y^(-1-1/n) * m = Mpa m/y
-    C_shear = stag4(B) * stag4(thk) *  tf.reduce_sum(weight * p_term, axis=1)
-
-    return C_shear
+    return stag4(B) * stag4(thk) *  tf.reduce_sum(weight * p_term, axis=1)
