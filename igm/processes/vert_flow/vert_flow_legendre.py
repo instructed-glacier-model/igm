52b2669ae4f6832260c137f3bdd5b83db1a8b6eb
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-07-15 16:26:52 +0200

    optimize compute_horizontal_derivatives

--- igm/processes/vert_flow/vert_flow_legendre.py
+++ igm/processes/vert_flow/vert_flow_legendre.py
@@ -13,24 +13,14 @@ from igm.utils.gradient.compute_gradient import compute_gradient
 from igm.processes.iceflow.utils import get_velbase
 
 def compute_vertical_velocity_legendre(cfg, state):
- 
+
     sloptopgx, sloptopgy = compute_gradient(state.topg, state.dX, state.dX, staggered_grid=False)
 
-    uvelbase, vvelbase = get_velbase(state.U, state.V, cfg.processes.iceflow.numerics.vert_basis) # Lagrange basis
+    uvelbase, vvelbase = get_velbase(state.U, state.V, cfg.processes.iceflow.numerics.vert_basis) 
 
     wvelbase = uvelbase * sloptopgx + vvelbase * sloptopgy # Lagrange basis
 
-    dUdx, dVdx, dUdy, dVdy = compute_horizontal_derivatives(
-                                state.U[None,...], state.V[None,...], 
-                                state.dX[None,...], staggered_grid=False 
-                                                           ) # Legendre basis
-
-    # dUdx = tf.einsum('ij,bjkl->bikl', state.P, dUdx) 
-    # dVdx = tf.einsum('ij,bjkl->bikl', state.P, dVdx)
-    # dUdy = tf.einsum('ij,bjkl->bikl', state.P, dUdy)
-    # dVdy = tf.einsum('ij,bjkl->bikl', state.P, dVdy)
-    
-    dUdx, dVdx, dUdy, dVdy = dUdx[0], dVdx[0], dUdy[0], dVdy[0]
+    dUdx, dVdx, dUdy, dVdy = compute_horizontal_derivatives(state.U, state.V, state.dX, staggered_grid=False) # Legendre basis
  
     # Lagrange basis
     WLA = wvelbase[None,...] \
