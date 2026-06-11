150583c3147afabc5bf84570ebeb36d673310cac
[sebastian rosier] <[sebastian.rosier@geo.uzh.ch]>
2026-06-11 12:17:03 +0200

    added new architecture option for testing

--- igm/processes/iceflow/emulate/utils/architectures/__init__.py
+++ igm/processes/iceflow/emulate/utils/architectures/__init__.py
@@ -2,10 +2,12 @@ from .cnns import CNN
 from .mlps import MLP
 from .nos import FNO
 from .dahunet import DahuNet
+from .chimera import Chimera
 
 Architectures = {
     "CNN":     CNN,
     "MLP":     MLP,
     "FNO":     FNO,
     "dahunet": DahuNet,
+    "chimera": Chimera,
 }
