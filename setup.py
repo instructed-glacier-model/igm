9a71e7ef71faa16d89e2a7414be53f31314a1e61
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2026-06-25 12:06:29 +0200

    Release 3.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="igm-model",
-    version="3.1.1",
+    version="3.1.2",
     author="Guillaume Jouvet",
     author_email="guillaume.jouvet@unil.ch",
     url="https://github.com/jouvetg/igm",
