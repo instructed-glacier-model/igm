e52ac55701cda6be755f53bf82f0bfc432b73fab
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2026-09-25 20:14:49 +0200

    oggm_shop: keep RGI folder by default, fix tidewater relabelling, warn on empty GlaThiDa / RGI6 sub_entity_mask, drop unused glathida_server

--- igm/inputs/oggm_shop/arrange_data.py
+++ igm/inputs/oggm_shop/arrange_data.py
@@ -13,6 +13,12 @@ import scipy.signal
 from igm.inputs.oggm_shop.read_glathida import read_glathida
 from igm.inputs.oggm_shop.masks_subentities import process_masks_subentities
  
+def _warn(state, msg):
+    if hasattr(state, "logger"):
+        state.logger.warning(msg)
+    else:
+        print("WARNING: " + msg)
+
 def arrange_data(cfg, state, path_RGI, ds, RGI_version, RGI_product):
  
     # Prepare output dictionary
@@ -30,7 +36,9 @@ def arrange_data(cfg, state, path_RGI, ds, RGI_version, RGI_product):
     ds_vars["thk"] = thk
 
     # Process masks
-    if (cfg.inputs.oggm_shop.sub_entity_mask) & (RGI_version == 7):
+    if cfg.inputs.oggm_shop.sub_entity_mask and RGI_version != 7:
+        _warn(state, "sub_entity_mask is only available for RGI 7: ignored.")
+    if cfg.inputs.oggm_shop.sub_entity_mask and RGI_version == 7:
         mask_vars = process_masks_subentities(ds, cfg, RGI_product, path_RGI)
         ds_vars.update(mask_vars)
     else:
@@ -76,6 +84,8 @@ def arrange_data(cfg, state, path_RGI, ds, RGI_version, RGI_product):
             thkobs = xr.DataArray(np.where(ds_vars["icemask"], thkobs, np.nan), dims=("y", "x"))
         else:
             thkobs = np.full(ds["topo"].shape, np.nan)
+        if np.all(np.isnan(thkobs)):
+            _warn(state, "No GlaThiDa thickness data for this glacier: thkobs is empty.")
     else:
         thkobs = np.full(ds["topo"].shape, np.nan)   
     ds_vars["thkobs"] = thkobs
